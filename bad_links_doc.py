3449854201a34a5a8ad1ba25780096738e5feca9
Biying Ling <111700877+s01134601@users.noreply.github.com>
2023-02-03 16:48:36 -0600

    Update bad_links_doc.py

--- bad_links_doc.py
+++ bad_links_doc.py
@@ -53,23 +53,20 @@ def get_url_status(doc_url_df_total):
     return doc_url_df_total
 
 
-if '__main__' == __name__:
+# if '__main__' == __name__:
 
+def detect_url_doc(input_dir_path, output_folder_path):
     # find doc and docx files
     requests.packages.urllib3.disable_warnings()
-    source_dir = 'C:/Users/biyingling/OneDrive - Cook County Government/OneDrive_2022-11-28'
-    doc_list = gen_doc_list(source_dir)
-    #print(doc_list)
-
+    doc_list = gen_doc_list(input_dir_path)
     # find urls inside files
     doc_url_df_total = pd.DataFrame()
     for doc_path in doc_list:
         doc_url_df = extract_url_from_doc(doc_path)
         doc_url_df_total = pd.concat([doc_url_df_total, doc_url_df])
     doc_url_df_total = get_url_status(doc_url_df_total)
-    print(doc_url_df_total)
+    # print(doc_url_df_total)
     doc = doc_url_df_total.drop_duplicates()
-    doc['doc_path'] = doc['doc_path'].str.replace('C:/Users/biyingling/OneDrive - Cook County Government', '')
     doc = doc.loc[doc['status'] != 200]
+    doc.to_csv(os.path.join(output_folder_path, "broken_links_docx.csv"), index=False)
 
-doc.to_csv('C:/Users/biyingling/OneDrive - Cook County Government/OneDrive_2022-11-28/broken_list_doc.csv')
